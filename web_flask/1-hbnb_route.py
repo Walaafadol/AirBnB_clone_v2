65987a9b98aa69dd9b783fa5379fdb81a0b9ae6e
walaafadol <walaafadul7@gmail.com>
2024-03-23 16:02:18 -0700

    ll

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" 0. Script to start a Flask web application """
+""" 1. Script to start a Flask web application with 2 commands """
 
 from flask import Flask
 
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns some text. """
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hello():
