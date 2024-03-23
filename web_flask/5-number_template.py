09091e259bf1f873d5c73f51b67d687fce3402a7
walaafadol <walaafadul7@gmail.com>
2024-03-23 16:20:57 -0700

    dd

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" 3. Add third view func that redirects and has default val for variable """
+""" 5. Add fifth view func that displays HTML page if n is int """
 
 from flask import Flask
+from flask import render_template
 
 
 app = Flask(__name__)
@@ -33,6 +34,20 @@ def python_text(text='is cool'):
     """ replace more text with another variable. """
     text = text.replace('_', ' ')
     return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>')
+def number_text(n):
+    """ replace with int only if given int. """
+    n = str(n)
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>')
+def html_num(n):
+    """ display html if n is int. """
+    n = str(n)
+    return render_template('5-number.html', n=n)
 
 
 if __name__ == '__main__':
