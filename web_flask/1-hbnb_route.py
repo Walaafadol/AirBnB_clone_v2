6416ab5609262ccac713fbead3276e4cf5abd1fd
walaafadol <walaafadul7@gmail.com>
2024-03-23 16:01:05 -0700

    kk

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,7 +6,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/', strict_slashes=False)
 def hello_world():
     """ Returns some text. """
@@ -16,5 +15,6 @@ def hello_world():
 def hello():
     """ Return other text. """
     return 'HBNB'
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
